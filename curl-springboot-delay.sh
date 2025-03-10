e3a9a4e87f9da11cdb3809521975957d82f0630a
김경준 <107162956+kyeongjun-dev@users.noreply.github.com>
2025-03-10 23:40:02 +0900

    Update curl-springboot-delay.sh

--- curl-springboot-delay.sh
+++ curl-springboot-delay.sh
@@ -1,7 +1,10 @@
+port=$1
 while true
 do
-    curl -s "localhost:30008/delay?delay=1" > /dev/null & 
-    curl -s "localhost:30008/delay?delay=3" > /dev/null &
-    curl -s "localhost:30008/delay?delay=5" > /dev/null &
+    curl -s "localhost:$port" > /dev/null &
+    curl -s "localhost:$port/ip" > /dev/null &
+    curl -s "localhost:$port/delay?delay=1" > /dev/null &
+    curl -s "localhost:$port/delay?delay=3" > /dev/null &
+    curl -s "localhost:$port/delay?delay=5" > /dev/null &
     sleep 1
 done
